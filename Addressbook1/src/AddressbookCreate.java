13c74e267e4ffa087a2adc9649940223e23ab2eb
“siddiq76” <siddiq.khan0786@gmail.com>
2021-02-16 18:44:54 +0530

    add

---
+++ Addressbook1/src/AddressbookCreate.java
@@ -0,0 +1,84 @@
+
+
+	public class AddressbookCreate{
+	public static void main(String[] args) {
+		Contact contact=new Contact("jaga", "c", "42, mandalpati", "chinnai", "Inida", 9898989, 1223243145,"jag@gmail.com");
+		System.out.println("New Contact has been create in address book.\n"+contact);
+	}	
+}
+
+class Contact{
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private int zip;
+	private long phoneNumber;
+	private String email;
+	public Contact(String firstName, String lastName, String address, String city, String state, int zip,
+			long phoneNumber,String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email=email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "Contact: "+firstName + lastName+ ", "+address+", "+city+", " +state+ ", "+", " +zip+ ", "+ phoneNumber + "\n"+email+".";
+	}
+	
+}
